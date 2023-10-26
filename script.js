80bb0106760ad1d22f99069d6353dfe5668340ec
Laxman Piraniya <rayarlaxman@gmail.com>
2023-10-26 10:39:13 +0530

    added all files

---
+++ script.js
@@ -0,0 +1,78 @@
+//01
+var empty = {};
+//02
+logMessage("Hello,my name is pirani.what is your name");
+function logMessage(message) {
+    console.log(message);
+  }
+
+  //03
+  function greet(name) {
+    console.log(`Hello, ${name}!`);
+}
+greet("Hello Nithu Welcome to the coding school"); 
+
+//04
+//person.firstName.  write javascript program to add a new property to an exisiting object.properties dot notation and bracket notation.
+//person["firstName"]
+
+//05
+let emailArray = [];
+emailArray.push("jhon@example.com");
+
+  //06
+  const myObject = {
+    hell01: "hi",
+    hello2:"hi" ,
+    hello3: "kn"
+  };
+  
+  delete myObject.hello2;
+  console.log(myObject);
+  //
+
+//07
+
+
+
+//08
+var x =("students");
+var Y =("course");
+var join =x.concat(Y);
+console.log(join)
+
+//09
+if ('address' in myObject) {
+    console.log('Address is there');
+  } else {
+    console.log('Address not found');
+  }
+  
+//10
+
+const book = {
+  title: "BLACK BERRY",
+  author: "Oliver",
+  year: 1882
+};
+const bookJSON = JSON.stringify(book);
+console.log(bookJSON);
+   
+//11
+const arraystudents = [
+    { name: "Kavi" },
+    { name: 12 },
+    { gpa:3 },
+    
+  ];
+  console.log(arraystudents)
+  
+//create an arry of object called students. Each object in the array should have the following properties.
+
+
+
+
+
+
+
+  
